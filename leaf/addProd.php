ddaf374e2ab83e091e7750247670c4bd914e850d
Kevincamelo5 <92350827+Kevincamelo5@users.noreply.github.com>
2023-11-29 15:58:24 -0600

    Update addProd.php
    se hicieron modificaciones al tipo de producto y se añadieron la seleccion de generos la parte de que dependiendo lo que se escoja en tipo de produto saldran los requerimentos relacionados aun esta pendiente

--- leaf/addProd.php
+++ leaf/addProd.php
@@ -11,11 +11,31 @@
     <body>  <!--permite hacer el registro de los productos-->
         <section>
             <form action="" method="post">
-                <fieldset>
-                    <legend for="tipo de producto">tipo de producto</legend>
-                    <input type="radio" name="libro" id="libro">libro
-                    <input type="radio" name="otro" id="otro">otro producto
+            <fieldset>
+                    <label for="productos">Selecciona un tipo de producto</label>
+                    <select name=producto id="producto">
+                        <optio value="S/C"></option>
+                        <optio value="libro"></option>
+                        <optio value="juego"></option>
+                        <optio value="papelería"></option>
+                        <optio value="audioLibro"></option>
+                        <optio value="ebook"></option>
+                    </select>
+            </fieldset>
+
+            <fieldset>
+                    <label for="generosLiterarios">Selecciona un tipo de producto</label>
+                    <select name=gerero id="genero">
+                        <optio value="S/G"></option>
+                        <optio value="horror"></option>
+                        <optio value="romance"></option>
+                        <optio value="drama/misterio"></option>
+                        <optio value="comic/manga"></option>
+                        <optio value="accion/aventura"></option>
+                        <optio value="ciencia ficción/fantasía"></option>
+                    </select>
                 </fieldset>
+
                 <fieldset>
                     <label for="imagen">Sube una imagen:</label>
                     <input type="file" id="imagen" name="imagen" accept="image/*">
